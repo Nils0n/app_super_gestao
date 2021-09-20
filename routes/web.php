d89f33867cd93129d181c6166483e07d239c3d83
nils0n <nilson_jose.v@hotmail.com>
2021-09-19 22:51:54 -0300

    feat:app

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\SobreNosController;
+
+
+
+
+
+Route::get('/', [PrincipalController::class , 'index'])->name('site.index');
+Route::get('/contato', [ContatoController::class , 'index'])->name('site.contato');
+Route::post('/contato', [ContatoController::class , 'index'])->name('site.contato');
+
+Route::get('/sobrenos', [SobreNosController::class , 'index'])->name('site.sobrenos');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'clientes';});
+    Route::get('/fornecedores', function(){return 'fornecedores';});
+    Route::get('/produtos', function(){return 'produtos';});
+});
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. clique <a href="'.route('site.index').'">aqui </a> para voltar a página inicial.';
+});
+
